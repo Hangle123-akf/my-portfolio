5d8e16c80766cfc3028cbd8cba0a7fd42ee4d881
Hangle123-akf <lehang632004@gmail.com>
2026-02-03 16:06:48 +0700

    push code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+
+const lantern = document.getElementById('lantern');
+const images = lantern.querySelectorAll('img');
+
+const radius = 120;
+let angle = 0;
+let isPaused = false;
+
+images.forEach((img, index) => {
+    const rotateY = (360 / images.length) * index;
+    img.style.transform =
+        `rotateY(${rotateY}deg) translateZ(${radius}px) translate(-50%, -50%)`;
+
+    img.addEventListener('mouseenter', () => isPaused = true);
+    img.addEventListener('mouseleave', () => isPaused = false);
+});
+
+function rotateLantern() {
+    if (!isPaused) {
+        angle += 0.3;
+        lantern.style.transform = `rotateY(${angle}deg)`;
+    }
+    requestAnimationFrame(rotateLantern);
+}
+
+rotateLantern();
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Portfolio của Nguyễn Nhật Duy đã load xong!');
 
